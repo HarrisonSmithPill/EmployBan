5b8bebaab6855f0cff15dcfb57ea4d84a00a4eaf
harrywardy <103024559+harrywardy@users.noreply.github.com>
2022-04-06 20:36:49 +1000

    Create AddData.h

---
+++ AddData.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+
+class ResistorBase
+{
+  private:
+  
+  int fAge;
+  char fFName;
+  char fSName;
+  char fPurchaseHistory []
+  
+  
+  
+  public:
+  
+  AddData(int fAge = 0);
+  
+  ~AddData(){}
+  
+  const int getAge() const;
+
+}
