77d7732540233797a9005710f39be2952603a0b0
harrywardy <103024559+harrywardy@users.noreply.github.com>
2022-04-06 20:59:58 +1000

    Update AddData.cpp

--- AddData.cpp
+++ AddData.cpp
@@ -1,11 +1,47 @@
 namespace std;
 #using "AddData.h"
 
-AddData :: AddData 
-
+AddData :: AddData :
+   fAge(0);
+   fFName("null");
+  fSName("null");
+  fDOB("null");
+  fID(0);
 {}
 
 AddData :: ~AddData
 {
-
+  delete[]  fAge, fFName, fSName, fDOB, fID;
 }
+
+  const int AddData :: getAge() const
+  {
+    return fAge;
+  }
+  
+  const char AddData :: getFName() const;
+  {
+    return fFName;
+  }
+  
+  const char AddData ::getLName() const;
+  {
+    return fLName;
+  }
+  
+  const char AddData :: getDOB() const;
+  {
+    return fDOB;
+  }
+  
+  const int AddData :: getID() const;
+  {
+    return fID;
+  }
+  
+  const char AddData :: insertProduct(const char fproductHistory[]);
+  {
+  }
+  
+
+  
