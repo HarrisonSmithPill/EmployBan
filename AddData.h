3083c575de4e0cd81dd360c168cb6ad416026014
HarrisonSmithPill <103024514+HarrisonSmithPill@users.noreply.github.com>
2022-04-09 11:23:40 +1000

    Update AddData.h

--- AddData.h
+++ AddData.h
@@ -2,44 +2,46 @@
 
 #include <iostream>
 #include <string>
+#include <fstream>
 
 class AddData
 {
-  private:
-  
-  int fAge;
-  char fFName;
-  char fSName;
-  char fDOB;
-  int fID;
-  
-  
-  public:
-  
-  AddData();
-  
-  ~AddData(){}
-  
-  const int getAge() const;
-  
-  const char getFName() const;
-  
-  const char getLName() const;
-  
-  const char getDOB() const;
-  
-  const int getID() const;
-  
-  const char insertProduct(const char fproductHistory[], char fProduct, int fsize);
-  
-  const int updateAge(int fupdate) const;
-  
-  const char updateFName(char fupdate) const;
-  
-  const char updateLName(char fupdate) const;
-  
-  const char updateDOB(char fupdate) const;
-  
-  const int updateID(int fupdate) const;
+private:
+
+	int fAge;
+	char fFName;
+	char fSName;
+	char fDOB;
+	int fID;
+	int OrderSelect;
+
+
+public:
+
+	AddData();
+
+	~AddData() {}
+
+	const int getAge() const;
+
+	const char getFName() const;
+
+	const char getLName() const;
+
+	const char getDOB() const;
+
+	const int getID() const;
+
+	const char insertProduct(const char fproductHistory[], char fProduct, int fsize);
+
+	const int updateAge(int fupdate) const;
+
+	const char updateFName(char fupdate) const;
+
+	const char updateLName(char fupdate) const;
+
+	const char updateDOB(char fupdate) const;
+
+	const int updateID(int fupdate) const;
 
 }
